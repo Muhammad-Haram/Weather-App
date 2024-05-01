14b8d44772f0c6ad0859e799aa4c6d89355ffd88
Muhammad Haram <haramh643@gmail.com>
2024-05-01 16:09:24 +0500

    ftch api

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,11 +1,18 @@
 "use client"
 import { IoSearch } from "react-icons/io5";
+interface InputProps {
+    handleSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void
+    setLocation: React.Dispatch<React.SetStateAction<string>>;
+}
 
-const Input = () => {
+const Input = ({ handleSearch, setLocation }: InputProps) => {
     return (
         <form className="flex items-center md:w-2/4 w-full order-2 md:order-1"
         >
-            < input type="text" className="w-full rounded-sm p-1 outline-none text-black" placeholder="Search..." />
+            < input type="text" className="w-full rounded-sm p-1 outline-none text-black" placeholder="Search..."
+                onKeyDown={handleSearch}
+                onChange={(e) => { setLocation(e.target.value) }}
+            />
 
             <div className="ml-[-25px] text-black cursor-pointer">
                 <IoSearch />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,48 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import Input from './components/Input'
 
 const page = () => {
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=2e59c87adc894a0f801183150243004&q=${location}&days=7&aqi=yes&alerts=yes`
+
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      try {
+        let response = await fetch(url)
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setData(data);
+        setLocation("");
+        setError("")
+      }
+      catch (error) {
+        setError("City Not found")
+        setData({});
+      }
+    }
+  }
+
+
   return (
     <div className="bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-fit">
       <div className="bg-white/25 w-full rounded-lg flex flex-col h-fit">
 
         <div className="flex flex-col md:flex-row justify-between items-center p-12">
-          <Input />
+          <Input handleSearch={handleSearch} setLocation={setLocation} />
           <h1 className="mb-8 md:mb-0 order-1 text-white text-2xl py-2 px-4 rounded-xl italic font-bold">
             Weather App.
           </h1>
         </div>
-
+        {data.current ? <div>{data.current.temp_f}</div> : null}
       </div>
     </div>
   )
