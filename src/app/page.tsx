838c19be14598046927264212270d71f1eb1f595
Muhammad Haram <haramh643@gmail.com>
2024-05-03 11:57:29 +0100

    security concern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
   const [data, setData] = useState({})
   const [location, setLocation] = useState("")
   const [error, setError] = useState("")
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=2e59c87adc894a0f801183150243004&q=${location}&days=7&aqi=yes&alerts=yes`
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=2e59c87adc894a0f801183150243004&q=${location}&days=7&aqi=yes&alerts=yes`
 
   const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
